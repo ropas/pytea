be116a9f483cf81ee84f201091d4d2698b3eabf3
HYJhoo <hoyoung.jhoo@sf.snu.ac.kr>
2021-04-07 11:33:39 +0900

    add sampler

--- packages/pytea/pylib/torch/utils/data/__init__.py
+++ packages/pytea/pylib/torch/utils/data/__init__.py
@@ -1,3 +1,4 @@
+from .sampler import Sampler
 from .dataset import Dataset, Subset, TensorDataset, random_split
 from .dataloader import DataLoader
 from . import distributed as distributed
---
+++ packages/pytea/pylib/torch/utils/data/sampler.py
@@ -0,0 +1,6 @@
+class Sampler():
+    def __init__(self, data_source):
+        pass
+
+    def __iter__(self):
+        raise NotImplementedError
