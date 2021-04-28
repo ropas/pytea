8d86d5280bed36fae7eb0842ea6ae0ff7ca7939a
unknown <shkim@ropas.snu.ac.kr>
2021-04-29 02:55:02 +0900

    sklearn

--- packages/pytea/pylib/sklearn/model_selection/split.py
+++ packages/pytea/pylib/sklearn/model_selection/split.py
@@ -1,5 +1,6 @@
 import LibCall
 import torch
+import torch.utils.data as data
 import torch.utils.data.dataset as dataset
 
 
@@ -7,10 +8,10 @@ import torch.utils.data.dataset as dataset
 def train_test_split(*arrays, **options):
     ret_arrays = []
 
-    #test_size = options.pop("test_size", None)
-    #train_size = options.pop("train_size", None)
+    # test_size = options.pop("test_size", None)
+    # train_size = options.pop("train_size", None)
 
-    test_size = options['test_size']
+    test_size = options["test_size"]
     train_size = None
 
     if test_size is None and train_size is None:
@@ -18,7 +19,25 @@ def train_test_split(*arrays, **options):
 
     if test_size is None:
         test_size = 1 - train_size
-    
+
+    # assumes arrays = (data_batch, label_batch)
+    # when arrays = [dataset(like MNIST)]
+    if isinstance(arrays[0], data.Dataset):
+        dset = arrays[0]  # dataset
+        dlen = len(dset)  # data length
+        test_length = int(test_size * dlen)
+        train_length = dlen - test_length
+
+        train = LibCall.shape.repeat(
+            dset[0][0], 0, train_length
+        )  # shape: (train_len, data_shape)
+        test = torch.Tensor(test_length)  # shape: (test_len)
+
+        ret_arrays.append(train)
+        ret_arrays.append(test)
+
+        return ret_arrays
+
     for array in arrays:
         test_length = int(test_size * len(array))
         train_length = len(array) - test_length
@@ -30,5 +49,5 @@ def train_test_split(*arrays, **options):
 
         ret_arrays.append(train)
         ret_arrays.append(test)
-    
+
     return ret_arrays
--- packages/pytea/pytest/tests/mnistex.py
+++ packages/pytea/pytest/tests/mnistex.py
@@ -3,25 +3,31 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
 
-dataroot = "../../../data"
+
+dataroot = "."
 
 transform = transforms.Compose(
     [transforms.ToTensor(), lambda x: torch.reshape(x, (784,)),]
 )
 
-# mnist = datasets.CIFAR10(root=dataroot, train=True, transform=None, download=True)
+cfar = datasets.CIFAR10(root=dataroot, train=True, transform=None, download=True)
 train_set, valid_set = train_test_split(
     datasets.MNIST(root=dataroot, train=True, transform=transform, download=True),
     test_size=0.1,
 )
+print(len(train_set))
+print(len(valid_set))
+print(train_set[0])
+print(valid_set[0])
 
-t = torch.rand(7, 3, 4)
-t0 = 1
-t1 = 1
-arr = []
 """
-for array in mnist:
-    t0 = array
-    t1 = t0[1]
-    arr.append(array)
+print(cfar)
+print("----------------")
+arr = []
+for array in cfar:
+    print(array)
+    print(len(array))
+    print(array[1])
+    arr.append(1)
+    break
 """
